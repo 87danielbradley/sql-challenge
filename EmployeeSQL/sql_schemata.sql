35d45df16bf2ec040e6bfa85afdeec3747999c85
Daniel Bradley <87danielbradley@gmail.com>
2020-04-14 11:30:00 -0400

    Final update

--- EmployeeSQL/sql_schemata.sql
+++ EmployeeSQL/sql_schemata.sql
@@ -1,57 +1,65 @@
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS dept_emp;
-DROP TABLE IF EXISTS dept_manager;
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS salaries;
-DROP TABLE IF EXISTS titles;
-
+DROP TABLE IF EXISTS dept_emp CASCADE;
+DROP TABLE IF EXISTS dept_manager CASCADE;
+DROP TABLE IF EXISTS salaries CASCADE;
+DROP TABLE IF EXISTS titles CASCADE;
+DROP TABLE IF EXISTS departments CASCADE;
+DROP TABLE IF EXISTS employees CASCADE;
 
 CREATE TABLE departments (
   dept_no VARCHAR(30) NOT NULL,
-  dept_name VARCHAR(30) NOT NULL
+  dept_name VARCHAR(30),
+  PRIMARY KEY (dept_no)
   );
+
+CREATE TABLE employees (
+    emp_no INTEGER NOT NULL,
+	birth_date DATE,
+    first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	gender VARCHAR(1),
+    hire_date DATE,
+	PRIMARY KEY (emp_no)
+);
 
 CREATE TABLE dept_emp (
   emp_no INTEGER NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
   dept_no VARCHAR(30) NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL
+  from_date DATE,
+  to_date DATE
 );
 
 CREATE TABLE dept_manager (
   dept_no VARCHAR(30) NOT NULL,
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
   emp_no INTEGER NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  from_date DATE,
+  to_date DATE
 );
 
-CREATE TABLE employees (
-    emp_no INTEGER NOT NULL,
-	birth_date DATE NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-	last_name VARCHAR(30) NOT NULL,
-	gender VARCHAR(1) NOT NULL,
-    hire_date DATE NOT NULL
-);
+
 
 CREATE TABLE salaries (
   emp_no INTEGER NOT NULL,
-  salary INTEGER NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  salary INTEGER,
+  from_date DATE,
+  to_date DATE
 );
 
 CREATE TABLE titles (
   emp_no INTEGER NOT NULL,
-  title VARCHAR(30) NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  title VARCHAR(30),
+  from_date DATE,
+  to_date DATE
 );
 
-COPY titles FROM 'C:\Users\chand\Documents\GitHub\sql-challenge\EmployeeSQL\titles.csv' DELIMITER ',' CSV HEADER;
 COPY employees FROM 'C:\Users\chand\Documents\GitHub\sql-challenge\EmployeeSQL\employees.csv' DELIMITER ',' CSV HEADER;
-COPY salaries FROM 'C:\Users\chand\Documents\GitHub\sql-challenge\EmployeeSQL\salaries.csv' DELIMITER ',' CSV HEADER;
 COPY departments FROM 'C:\Users\chand\Documents\GitHub\sql-challenge\EmployeeSQL\departments.csv' DELIMITER ',' CSV HEADER;
+COPY titles FROM 'C:\Users\chand\Documents\GitHub\sql-challenge\EmployeeSQL\titles.csv' DELIMITER ',' CSV HEADER;
+COPY salaries FROM 'C:\Users\chand\Documents\GitHub\sql-challenge\EmployeeSQL\salaries.csv' DELIMITER ',' CSV HEADER;
 COPY dept_emp FROM 'C:\Users\chand\Documents\GitHub\sql-challenge\EmployeeSQL\dept_emp.csv' DELIMITER ',' CSV HEADER;
 COPY dept_manager FROM 'C:\Users\chand\Documents\GitHub\sql-challenge\EmployeeSQL\dept_manager.csv' DELIMITER ',' CSV HEADER;
 
